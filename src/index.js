331795808d0677dd9ebc92d932413a92497da5d2
Manikanta Reddy <manikantar585@gmail.com>
2026-08-27 16:13:19 +0530

    Updated React entry configuration

--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
 import "./index.css";
 import App from "./App";
-
 import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
-
 import theme from "./theme/theme";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
